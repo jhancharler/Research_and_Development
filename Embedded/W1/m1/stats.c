2b09cf05f85a62860e85c7b1cd5b75070812f454
Jan Charler <charlerjan@gmail.com>
2020-10-24 20:40:55 +0100

    feat: all functions complete. merge sort implemented

--- Embedded/W1/m1/stats.c
+++ Embedded/W1/m1/stats.c
@@ -25,19 +25,26 @@ void main() {
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
-  print_array(test, SIZE);
-
-  printf("Mean = %i\n", find_mean(test, SIZE)); 
-
-  printf("Max = %i\n", find_maximum(test, SIZE)); 
-
-  printf("Min = %i\n", find_minimum(test, SIZE)); 
-
+  // Print min, max, mean, median and the sorted array.
+  print_statistics(test, SIZE);
 
 }
 
-void print_statistics(unsigned char arr[], int size)
-{}
+void print_statistics(unsigned char arr[], int size){
+
+  print_array(arr, size);
+
+  printf("Min = %i\n", find_minimum(arr, size));
+
+  printf("Max = %i\n", find_maximum(arr, size)); 
+
+  printf("Mean = %i\n", find_mean(arr, size)); 
+
+  printf("Median = %i\n", find_median(arr, size)); 
+
+  print_array(arr, size);
+
+}
 
 // prints contents of array
 void print_array(unsigned char arr[], int size){ 
@@ -52,11 +59,14 @@ void print_array(unsigned char arr[], int size){
  unsigned char find_median(unsigned char arr[], int size){
 
  	// Sort array
+ 	sort_array(arr, size);
 
  	// Go to middle
+ 	int middle = size / 2;
+
+ 	return arr[middle];
  }
 
- //calculates and returns mean
  unsigned char find_mean(unsigned char arr[], int size){
 
  	unsigned int sum = 0;
@@ -97,6 +107,64 @@ void print_array(unsigned char arr[], int size){
  	return min;
  }
 
- unsigned char* sort_array(unsigned char arr[], int size){
+ void sort_array(unsigned char arr[], int size){
 
+ 	printf("Sorting array.\n");
+ 	merge_sort(arr, 0, size-1);
  }
+
+ void merge_sort(unsigned char arr[], int start, int end){
+
+ 	if (start >= end) return;
+
+ 	int middle = start + ((end - start)/2);
+
+ 	merge_sort(arr, start, middle); // sort left side
+ 	merge_sort(arr, middle+1, end); // sort right side
+
+ 	merge(arr, start, middle, end);
+
+ 	return;
+ }
+
+ void merge(unsigned char arr[], int start, int middle, int end){
+
+ 	unsigned char tempArr[SIZE];
+
+ 	int i = start;
+ 	int j = middle+1;
+ 	int pos = start;
+ 	while (i < middle+1 && j < end+1){
+
+ 		// Merge
+		if (arr[i] < arr[j]){
+			tempArr[pos] = arr[i];
+			i++;
+			pos++;
+		}
+		else{
+			tempArr[pos] = arr[j];
+			j++;
+			pos++;
+		}
+ 	}
+
+ 	// Corner cases
+ 	while (i < middle+1){
+		tempArr[pos] = arr[i];
+		i++;
+		pos++;
+ 	}
+
+ 	while (j < end+1){
+		tempArr[pos] = arr[j];
+		j++;
+		pos++;
+ 	}
+
+ 	for (int i = start; i < end+1; i++){
+ 		arr[i] = tempArr[i];
+ 	}
+ }
+
+
--- Embedded/W1/m1/stats.h
+++ Embedded/W1/m1/stats.h
@@ -77,13 +77,43 @@ void print_statistics(unsigned char arr[], int size);
  unsigned char find_minimum(unsigned char arr[], int size);
 
  /**
- * @brief Sort and returns array using ...
+ * @brief Sort and returns array using merge sort
  *
  * @param arr The array to be analysed.
  * @param size The size of array.
  *
  * @return The pointer to the start of the array.
  */
- unsigned char* sort_array(unsigned char arr[], int size);
+ void sort_array(unsigned char arr[], int size);
+
+  /**
+ * @brief Helper function to sort_array
+ *
+ * This is called recursively. Applies the merge sort algorithm.
+ * The function is called with smaller and smaller start/end interval
+ *
+ * @param arr The array to be analysed.
+ * @param start The starting point of array to sort
+ * @param end The end of array to sort
+ *
+ * @return Void
+ */
+ void merge_sort(unsigned char arr[], int start, int end);
+
+  /**
+ * @brief Merges two sorted arrays
+ *
+ * This is called recursively. Applies the merge sort algorithm.
+ * The function is called with smaller and smaller start/end interval
+ *
+ * @param arr The array to be analysed.
+ * @param size The size of array.
+ * @param start The starting point of first array
+ * @param middle The end point of first array and 1 less than start of second array
+ * @param end The end of second array
+ *
+ * @return Void
+ */
+ void merge(unsigned char arr[], int start, int middle, int end);
 
 #endif /* __STATS_H__ */
