d4d91d6d5b26e9cc5364d54c71c40783cccabcf9
Jan Charler <charlerjan@gmail.com>
2020-10-24 15:39:47 +0100

    feat: complete implementation for min, max, mean

--- Embedded/W1/m1/stats.c
+++ Embedded/W1/m1/stats.c
@@ -27,31 +27,76 @@ void main() {
 
   print_array(test, SIZE);
 
+  printf("Mean = %i\n", find_mean(test, SIZE)); 
+
+  printf("Max = %i\n", find_maximum(test, SIZE)); 
+
+  printf("Min = %i\n", find_minimum(test, SIZE)); 
+
+
 }
 
 void print_statistics(unsigned char arr[], int size)
 {}
 
-void print_array(unsigned char arr[], int size){
+// prints contents of array
+void print_array(unsigned char arr[], int size){ 
 
-	printf("[");
+	printf("Printing array:\n[");
 	for (int i = 0; i < size; i++){
 		printf("%i, ", arr[i]);
 	}
 	printf("\b\b]\n");	// remove the final comma and space
 }
 
- unsigned char find_median(unsigned char arr[], int size)
- {}
+ unsigned char find_median(unsigned char arr[], int size){
 
- unsigned char find_mean(unsigned char arr[], int size)
- {}
+ 	// Sort array
 
- unsigned char find_maximum(unsigned char arr[], int size)
- {}
+ 	// Go to middle
+ }
 
- unsigned char find_minimum(unsigned char arr[], int size)
- {}
+ //calculates and returns mean
+ unsigned char find_mean(unsigned char arr[], int size){
 
- unsigned char* sort_array(unsigned char arr[], int size)
- {}
+ 	unsigned int sum = 0;
+
+ 	for (int i=0; i<size; i++){
+ 		sum += (unsigned int)arr[i];
+ 	}
+
+ 	float ans = sum / size;
+
+ 	unsigned char avg = (unsigned char)ans;
+
+ 	return avg;
+ }
+
+ unsigned char find_maximum(unsigned char arr[], int size){
+
+ 	unsigned char max = 0;
+
+ 	for (int i = 0; i < size; i++){
+ 		if (arr[i] > max){
+ 			max = arr[i];
+ 		}
+ 	}
+
+ 	return max;
+ }
+
+ unsigned char find_minimum(unsigned char arr[], int size){
+ 	unsigned char min = 0xFF;
+
+ 	for (int i = 0; i < size; i++){
+ 		if (arr[i] < min){
+ 			min = arr[i];
+ 		}
+ 	}
+
+ 	return min;
+ }
+
+ unsigned char* sort_array(unsigned char arr[], int size){
+
+ }
